ab38894755df77839746a5c80df7d45b436b1ba9
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-24 04:58:26 +0000

    datatypes

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//ye naya version use kare ga
+// alert(3+3) we are using node not chrome v8
+ let name ="Arfat"
+ let age = 19
+ let isLoggedIn = true
+
+ // is mai java ke jesa nahi hai 
+//  number => 2^53 hai
+//  BigInt
+//  String=""
+//  Boolean
+//  null is Object
+ console.log(typeof null);
+ console.log(typeof undefined);
