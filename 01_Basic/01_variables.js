9a9866e3f348b068f317eef9aa261850c7bd559e
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-24 04:29:25 +0000

    variables

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=123 
+// all correct account_id account_ID
+let email = "sk@gmail.com" // har jaga let us karo
+var pass = "Shaikh" // is ka scope ka problem hai 
+locationName = "Jaipur" // bad practice hai 
+// coonstant change nahi hoga
+email="mk@gmail.com"
+pass ="Sk"
+locationName="MUM"
+let areaName; //it will show undefine
+// console table for multiple values
+console.table([email,pass,locationName,areaName])
