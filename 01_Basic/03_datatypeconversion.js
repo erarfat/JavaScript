6dceae18b9397b894a2bfd608d6ae5aeb4ea296f
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-24 06:33:08 +0000

    operations

--- 01_Basic/03_datatypeconversion.js
+++ 01_Basic/03_datatypeconversion.js
@@ -14,3 +14,19 @@ console.log("String with both "+inStrNumCon);
 console.log("BOOLEAN "+inBoolCon);
 console.log("NULL "+inNullCon);
 console.log("Undefined "+ inUndefine);
+
+// Note koi bhi cheez convert karne se pahile chech karo 
+
+// ==========> Operations <=============
+// all same hai +,-,/,%,*,(m**n) m rase to n hai
+
+// ye check karo
+console.log("1"+2+2); //string se chalu kiya to append karega
+console.log(2+2+"1");// number se kiya to add and string wala appende
+
+//perefix and postfix
+let a=10
+let b=10
+let c= ++a + (b++)
+//      11 + 10 b++ ke bad jese he code console pe jaye ga to b increment hoga
+console.log(c);
