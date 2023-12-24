6d2c127d4bd8c9fcacd638f945d4e0fceaecc68e
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-24 06:07:57 +0000

    datatypes conversion

---
+++ 01_Basic/03_datatypeconversion.js
@@ -0,0 +1,16 @@
+// Any dataTypes to Number
+let inStr="33"
+let inStrNum="331a"
+let inBool=true
+let inNull=null
+let inUndefine=undefined
+let inStrCon=Number(inStr)
+let inStrNumCon=Number(inStrNum)
+let inBoolCon=Number(inBool)
+let inNullCon=Number(inNull)
+let inUndefineCon=Number(undefined)
+console.log("String only with number "+inStrCon);
+console.log("String with both "+inStrNumCon);
+console.log("BOOLEAN "+inBoolCon);
+console.log("NULL "+inNullCon);
+console.log("Undefined "+ inUndefine);
