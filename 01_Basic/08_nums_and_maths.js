aaadd6db09f2e6fe5a705e32be75632d599b3a26
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-26 14:16:39 +0000

    Maths and Numbers

---
+++ 01_Basic/08_nums_and_maths.js
@@ -0,0 +1,20 @@
+//+=======> Numbers <=======
+const number = 123
+const num = new Number(123) //obj 
+console.log(num);
+console.log(num.toString());
+const decm = 106.6239
+console.log(decm.toFixed(2));// imp
+
+console.log(decm.toPrecision(2));
+
+const hund = 1000000
+console.log(hund.toLocaleString('en-IN')); // add coma
+
+
+// ++++++++++ Maths++++++++++++++
+
+console.log(Math.random());
+
+// some if we want in range then
+
