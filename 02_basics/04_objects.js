127f65466b0cba64c7f245b1022ab016c05fc999
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-30 14:20:10 +0000

    ob

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -15,11 +15,75 @@ const myObj ={
     id: "sk@gmail.com",
     fullname: {
         username:{
-            firstname: "Arfta"
+            firstname: "Arfta",
             lastname: "Shaikh"
         }
     }
 }
 
 // how to access it
+console.log(myObj.fullname.username.firstname);
+
+// method to concate the object
+const firstObject = {
+    name: "Arfat",
+    email: "arfat@google.com"
+}
+
+const secondObject = {
+    id: 1,
+    loggedIn: true
+}
+
+// const thirdObject ={firstObject,secondObject}// ye same hai array.push()
+
+//const thirdObject = {...firstObject, ...secondObject} corret hai mostly used hai
+
+const thirdObject = Object.assign({},firstObject,secondObject) // note {} first mai hai uska matlab ye hai all object usme dalo nahi lagaye to bhi chalega
+
+console.log(thirdObject);
+
+// suppose in array there is may object
+
+const arr = [
+    {
+        name: "Arfat",
+        email: "arfat@google.com"
+    },
+    {
+        id: 1,
+        loggedIn: true
+    },
+    {
+
+    }
+]
+
+// how to accses is
+
+console.log(arr[0].email);
+
+// how retriview object key and values
+
+// console.log(jsUser.keys()); wrong
+
+console.log(Object.keys(jsUser));// returen array
+
+console.log(Object.keys(myObj));// return two array number object = number array
+
+console.log(Object.values(jsUser));// retuern values
+
+console.log(Object.entries(jsUser));// return array in key-values pairs
+
+console.log(Object.entries(myObj));// imp check
+
+
+// how to check wether key isthere or not imp
+
+console.log(myObj.hasOwnProperty('isLoggedIn'));
+
+console.log(secondObject.hasOwnProperty('loggedIn'));
+
+
+
 
