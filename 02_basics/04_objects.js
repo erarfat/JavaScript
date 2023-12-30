45f291434251da386eb968a488a5cd0723914db8
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-30 11:13:19 +0000

    upate

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+// object using constructor singleton
+// const myObj = new Object() constructor
+
+const jsUser = {}
+//console.log(jsUser);
+jsUser.id = "sk@gmail.com"
+jsUser.name = "Arfat"
+jsUser.isLoggedIn = true
+
+//console.log(jsUser);
+
+// nested object 
+
+const myObj ={
+    id: "sk@gmail.com",
+    fullname: {
+        username:{
+            firstname: "Arfta"
+            lastname: "Shaikh"
+        }
+    }
+}
+
+// how to access it
+
