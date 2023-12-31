499e07f3efba8a0580235e414dc6bd6468db5208
erarfat <154660646+erarfat@users.noreply.github.com>
2023-12-31 09:21:14 +0000

    function done

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -29,5 +29,74 @@ function calculateCartPrice3(val1, val2, ...num1){
 console.log(calculateCartPrice3(10,20,30,40,50));
 // op [ 30, 40, 50 ] 0, 1 th value val1 and val2 lega
 
+// passing object to function
+
+const myObje = {
+    username: "Arfat",
+    email: "sk@gmail.com"
+}
+
+function Objecttake(tkobj){
+    console.log(`The username is ${tkobj.username} and email is ${tkobj.email}`);
+}
+
+Objecttake(myObje)
+
+// suppose there is email not present then it will show undefine
+
+// Objecttake({
+//     username:"Arfat sk"
+// })
+
+//console.log(myObje.hasOwnProperty('isLoggedIn'));
+
+function objectTakeUpdate(tkobj){
+    if (tkobj.hasOwnProperty('username') && tkobj.hasOwnProperty('email') ) {
+        console.log(`The username is ${tkobj.username} and email is ${tkobj.email}`);
+    }
+    else if (tkobj.hasOwnProperty('username')) {
+        console.log("Plese enter email");
+    }
+    else{
+        console.log("Plese enter the username");
+    }
+    
+}
+
+console.log(objectTakeUpdate({
+    username: "Lala",
+    email: "lala@gmail.com"
+}));
+
+console.log(objectTakeUpdate({
+    username: "Lala",
+    //email: "lala@gmail.com"
+}));
+
+console.log(objectTakeUpdate({
+    //username: "Lala",
+    email: "lala@gmail.com"
+}));
+
+// takeing array
+
+const arr = [10,20,30,40]
+
+function printElementAtGnIndex(myArr,index){
+        return myArr[index]
+}
+
+console.log(printElementAtGnIndex(arr,2)); //30
+
+// inserting out value
+
+console.log(printElementAtGnIndex(arr,10)); // undefined
+
+
+
+
+
+
+
 
 
