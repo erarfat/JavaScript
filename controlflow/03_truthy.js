eef120c8bbaaf2a20b823fb3bb22ac844fdf28da
erarfat <154660646+erarfat@users.noreply.github.com>
2024-01-01 10:01:52 +0000

    done

--- controlflow/03_truthy.js
+++ controlflow/03_truthy.js
@@ -7,3 +7,24 @@
 
 //"0", " ", [], {}, function()
 
+// Nullish coalescing Operator (??): null , undefined
+
+let val;
+
+val = 5 ?? 10
+
+console.log(val);
+
+val1 = null ?? 10
+
+console.log( val1);
+
+val2 = null ?? 10 ?? 20 // ?? ke baad first valid  value lega
+
+console.log(val2);
+
+val4 = undefined ?? null ?? 100
+console.log(val4);
+
+// note above same hai null and undefined ke liye
+
